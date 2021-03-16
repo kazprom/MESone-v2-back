577c05465c887ebe1d960e99d1e6dd18845ffe58
Stanislav Ussanov <s.ussanov@mail.ru>
2021-03-16 09:52:04 +0600

    Добавить логику для настройки данных о компании

---
+++ database/migrations/2021_03_16_034300_create_companies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('company_name')->nullable();
+            $table->timestamp('launch_date', 0)->nullable();
+            $table->string('logo_path')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
